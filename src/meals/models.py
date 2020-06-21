13a902959ba0958defcdf03b36b2d29bd76d8c81
yogamahottama <yoga.mahottama@gmail.com>
2020-06-21 15:46:30 +0800

    feat(component): creating model meals

--- src/meals/models.py
+++ src/meals/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Meals(models.Model):
+    name =  models.CharField(max_length=50)
+    description = models.Textfield(max_length=500)
+    people = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    preparation_time = models.IntegerField()
+    image = models.ImageField(upload_to='meals/')
+
+    
